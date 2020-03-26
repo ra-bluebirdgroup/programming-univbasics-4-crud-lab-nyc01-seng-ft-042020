9ba398c6e51a7caff9dbb73adf307834ec56c320
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-03-26 23:14:53 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -4,26 +4,26 @@ end
 
 def create_an_array
   Array.new(4, " ")
-
 end
 
 def add_element_to_end_of_array(array, element)
-
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
-
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-
+ array.pop
 end
 
 def remove_element_from_start_of_array(array)
-
+ array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
+ array[index_number]
 
 end
 
